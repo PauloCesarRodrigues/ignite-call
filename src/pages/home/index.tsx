586c8e42ed4ea5cd59f37f08fb487c3e1a9f2e9b
Paulo Cesar Rodrigues <pauloceasrrtv321@gmail.com>
2025-02-25 14:51:29 -0300

    add: Home page styles

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,30 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Preview, Hero } from './styles'
+
+import previewImage from '@/assets/app-preview.png'
+import Image from 'next/image'
+import { ClaimUserNameForm } from './components/ClaimUsernameForm'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+        <ClaimUserNameForm />
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          alt="Calendário simbolizando a aplicação funcionando"
+          height={400}
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
