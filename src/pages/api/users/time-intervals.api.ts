d2b86f2d8cff0a307d9e5dcfd1570978d09734c5
Paulo Cesar Rodrigues <pauloceasrrtv321@gmail.com>
2025-03-03 20:33:27 -0300

    add: Authentified user time intervals route

---
+++ src/pages/api/users/time-intervals.api.ts
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number().min(0).max(6),
+      startTimeInMinutes: z.number().min(0).max(1439),
+      endTimeInMinutes: z.number().min(0).max(1439),
+    }),
+  ),
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  if (!session) res.status(401).end()
+
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session!.user.id,
+        },
+      })
+    }),
+  )
+  // await prisma.userTimeInterval.createMany <--- n da no SQLite
+
+  return res.status(201).end()
+}
